1ab0c561f3ffe67fc0f8946034da70c925c0c809
pirainogi <pirainogi@gmail.com>
2019-07-15 12:33:06 -0400

    cant hit the github api

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log('document ready');
+
+    getRepos = () => {
+      fetch('https://api.github.com/users/pirainogi/repos')
+      .then(r => r.json())
+      .then(data => console.log('fetch', data))
+    }
+
+    getRepos()
 });
